b8b001bfa81e9b0c319818c5ce9fc14c45056c0b
rolandparragh <34657233+rolandparragh@users.noreply.github.com>
2023-10-18 01:03:56 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,16 +1,23 @@
 import datetime
 
+# Calculate the current week
 current_week = datetime.date.today().isocalendar()[1]
-#week
+
+# Initialize the Markdown content
 markdown_content = f"""\
-| Week | Name | Contact details|
-|----| ----| ----------|
+| Week | Name | Contact details |
+| ---- | ---- | --------------- |
 """
+
+# Loop through the weeks (1 to 52)
 for week in range(1, 53):
     if week == current_week:
+        # Highlight the current week with **week**
         markdown_content += f"| **{week}** | XYZ | @I584002 |\n"
     else:
-        markdown_content += f"| {week} | Oncall name | Contact details| \n"
+        # For other weeks, you can specify on-call name and contact details
+        markdown_content += f"| {week} | Oncall name | Contact details |\n"
 
-        with open("table.md", "w") as f:
-            f.write(markdown_content)
+# Write the content to the Markdown file
+with open("table.md", "w") as f:
+    f.write(markdown_content)
