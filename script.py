cb4b80b0acdd2b53aa7c4d7412c924abf247c461
rolandparragh <34657233+rolandparragh@users.noreply.github.com>
2023-10-18 01:13:45 +0200

    Update script.py

--- script.py
+++ script.py
@@ -3,21 +3,39 @@ import datetime
 # Calculate the current week
 current_week = datetime.date.today().isocalendar()[1]
 
-# Initialize the Markdown content
-markdown_content = f"""\
-| Week | Name | Contact details |
-| ---- | ---- | --------------- |
+# Initialize the HTML content
+html_content = f"""\
+<table>
+  <tr>
+    <th>Week</th>
+    <th>Name</th>
+    <th>Contact details</th>
+  </tr>
 """
 
 # Loop through the weeks (1 to 52)
 for week in range(1, 53):
     if week == current_week:
-        # Indicate the current week with "Current" or any label you prefer
-        markdown_content += f"| **Current** (Week {week}) | XYZ | @I584002 |\n"
+        # Highlight the entire row with a yellow background
+        html_content += f"""\
+  <tr style="background-color: yellow;">
+    <td><strong>{week}</strong></td>
+    <td><strong>XYZ</strong></td>
+    <td><strong>@I584002</strong></td>
+  </tr>
+"""
     else:
-        # For other weeks, you can specify on-call name and contact details
-        markdown_content += f"| {week} | Oncall name | Contact details |\n"
+        # For other weeks, specify on-call name and contact details
+        html_content += f"""\
+  <tr>
+    <td>{week}</td>
+    <td>Oncall name</td>
+    <td>Contact details</td>
+  </tr>
+"""
+
+html_content += "</table>"
 
 # Write the content to the Markdown file
 with open("table.md", "w") as f:
-    f.write(markdown_content)
+    f.write(html_content)
